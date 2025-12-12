e399754df4076edb7ed594938d12f8ed64889e2a
unknown <gorapallijitendra2007@gmail.com>
2025-12-12 17:04:29 +0530

    running

--- index.tsx
+++ index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import ErrorBoundary from './components/ErrorBoundary';
 
+console.log("HELLO WORLD SCRIPT STARTING");
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
