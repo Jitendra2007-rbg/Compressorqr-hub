4cb7816367c283643cd73fafbf912cf5844b6084
unknown <gorapallijitendra2007@gmail.com>
2025-12-25 09:58:12 +0530

    running

--- server.js
+++ server.js
@@ -245,7 +245,7 @@ app.get('/share/:filename', (req, res) => {
 
 
 // Catch-all route to serve the React app
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
